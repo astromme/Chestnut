2e40c20f7f3560891bb2e946ba2ecfdde9780787
Ryan Carlson <rcarlso1@gmail.com>
2010-04-01 18:47:08 -0400

    added some CUDA stuff

--- CUDA/map_example.cu
+++ CUDA/map_example.cu
@@ -6,17 +6,20 @@
 // syntax highlighting and command completion.
 #include <cuda_runtime_api.h>
 
-
 // Prototypes
-template <typename FCN>
-__global__ void map(float* array, int cols, FCN fcn);
+template <typename TYPE, typename FCN>
+//template <typename TYPE>
+__global__ void map(TYPE *array, int cols, FCN fcn);
 
-float plusThree(float val);
-void printArray(float* array, int rows, int cols);
+template <typename TYPE>
+TYPE plus(TYPE addBy, TYPE val);
+
+template <typename TYPE>
+void printArray(TYPE* array, int rows, int cols);
 
 // Host function. Runs on CPU
 int main(int argc, char** argv) {
-  int N = 8; // Size of our square array;
+  int N = 3; // Size of our square array;
   float *host; // Memory on host (cpu) side
   float *dev; // Memory on device (gpu) side
   
@@ -34,7 +37,7 @@ int main(int argc, char** argv) {
   }
   
   printf("Initial Array\n");
-  printArray(host, N, N);
+  printArray<float>(host, N, N);
   
   // Copy the array host to dev. We must give it how many bytes to copy hence the sizeof()
   cudaMemcpy(dev, host, N*N*sizeof(float), cudaMemcpyHostToDevice);
@@ -43,41 +46,50 @@ int main(int argc, char** argv) {
   // 1 tells it to have one single block
   // dim3(N,N) tells it to have N by N threads in that block.
   // Give the grow kernel the 'dev' array and the number of columns (N)
-  map<<<1, dim3(N,N)>>>(dev, N, plusThree);
+
+  //map<float><<<1, dim3(N,N)>>>(dev, N);
+  
+  /*********************************
+   * THIS MAKES THE PROGRAM FAIL -- CAN'T DO IT, YO
+   *********************************/
+  // map<float><<<1, dim3(N,N)>>>(dev, N, plus<float>);
   
   // Once the kernel has run, copy back the 'dev' array to the 'host' array
   cudaMemcpy(host, dev, N*N*sizeof(float), cudaMemcpyDeviceToHost);
   
   // Now it should have incrementing numbers
   printf("After 'map' kernel:\n");
-  printArray(host, N, N);
+  printArray<float>(host, N, N);
   
   // free up the allocated memory on the device
   cudaFree(dev);
-  free(host);
   
   return 0;
 }
 
-template <typename FCN> 
-__global__ void map(float* array, int cols, FCN fcn) {
+template <typename TYPE, typename FCN> 
+//template <typename TYPE> 
+__global__ void map(TYPE *array, int cols, FCN fcn) {
   int row = threadIdx.x;
   int col = threadIdx.y;
   
   // set each slot to its index number
   //array[row*cols + col] += 3;
-  array[row*cols + col] = fcn(array[row*cols + col]);
+  array[row*cols + col] = fcn(3, array[row*cols + col]);
 }
 
-float plusThree(float val){
-  return val + 3;
+// note, TYPE must be a(n) int, long, float, double, char
+template <typename TYPE>
+__device__ TYPE plus(TYPE addBy, TYPE val){
+  return val + addBy;
 }
 
-void printArray(float* array, int rows, int cols) {
+template <typename TYPE>
+void printArray(TYPE* array, int rows, int cols) {
   for(int row=0; row<rows; row++) {
     for (int col=0; col<cols; col++) {
       // calculate the spot in the 2d array that we want.
-      float e = array[(row * cols) + col];
+      TYPE e = array[(row * cols) + col];
       printf("%.1f\t", e);
     }
     printf("\n");
