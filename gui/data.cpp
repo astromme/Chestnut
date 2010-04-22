94cb2446cd26091fb0baab15629c0e3746f468c2
Andrew Stromme <astromme@chatonka.com>
2010-04-22 18:35:56 -0400

    add temporary variable name generator

--- gui/data.cpp
+++ gui/data.cpp
@@ -9,3 +9,14 @@ Data::~Data()
 {
 
 }
+
+QString Data::tempData(Type t) {
+  static int counter = 0;
+  counter++;
+  switch (t) {
+    case Data::Value:
+      return QString("tempScalar%1").arg(counter);
+    case Data::DataBlock:
+      return QString("tempVector%1").arg(counter);
+  }
+}
