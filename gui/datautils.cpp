c6a612fa968a156cc1f2372c9fa5b405fe7739f8
Ryan Carlson <rcarlso1@gmail.com>
2010-04-24 00:08:07 -0400

    have flatten() doing some good stuff with declarations -- no executions yet

--- gui/datautils.cpp
+++ gui/datautils.cpp
@@ -4,6 +4,12 @@ const ProgramStrings operator+(const ProgramStrings& p1, const ProgramStrings& p
   return ProgramStrings(p1.first + p2.first, p1.second + p2.second);
 }
 
+const ProgramStrings operator+=(const ProgramStrings& p1, const ProgramStrings& p2)
+{
+  return ProgramStrings(p1 + p2);
+}
+
+
 
 
  
