01e24513cd9d9e7e4e6ff6713b3d327048c109ee
Andrew Stromme <astromme@chatonka.com>
2011-06-02 18:12:21 -0400

    add a minimal interpreter

--- parser/gui.py
+++ parser/gui.py
@@ -5,6 +5,19 @@ from PyQt4 import QtGui,QtCore
 from parser import parse
 from symboltable import SymbolTable
 from nodes import *
+import sys
+
+app = QtGui.QApplication(sys.argv)
+
+window = QtGui.QWidget()
+layout = QtGui.QHBoxLayout()
+
+codeWindow = QtGui.QPlainTextEdit()
+resultsWindow = QtGui.QPlainTextEdit()
+
+class GuiStream:
+  def write(self, text):
+    resultsWindow.appendPlainText(text)
 
 def evaluate(ast):
   print ast
@@ -13,28 +26,31 @@ def evaluate(ast):
   for program_node in ast:
     program_node.evaluate(global_env)
 
-
 def text_changed():
-    print "text changed"
-    #ast = parse(code)
-    #evaluate(ast)
+    try:
+      ast = parse(str(codeWindow.toPlainText()))
+    except:
+      return
+
+    old = sys.stdout
+    resultsWindow.clear()
+    sys.stdout = GuiStream()
+    evaluate(ast)
+    sys.stdout = old
 
 def main():
     import sys, os
     import shlex, subprocess
 
-    with open(sys.argv[1], 'r') as f:
-      code = ''.join(f.readlines())
+    layout.addWidget(codeWindow)
+    layout.addWidget(resultsWindow)
 
-    app = QtCore.QCoreApplication(sys.argv)
-
-    codeWindow = QtGui.QPlainTextEdit()
-    resultsWindow = QtGui.QPlainTextEdit()
+    window.setLayout(layout)
+    window.setWindowTitle("Interpreter")
 
     codeWindow.textChanged.connect(text_changed)
 
-    codeWindow.show()
-    resultsWindow.show()
+    window.show()
 
     app.exec_()
 
