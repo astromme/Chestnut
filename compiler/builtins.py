847ed225098b22e5a295f3238aa98e298db3a539
Andrew Stromme <astromme@chatonka.com>
2011-06-15 14:21:38 -0400

    add a sqrt builtin function

--- compiler/builtins.py
+++ compiler/builtins.py
@@ -2,7 +2,7 @@ from nodes import SequentialFunction, Parameter
 
 #SequentialFunction = namedtuple('SequentialFunction', ['name', 'type', 'parameters', 'ok_for_device', 'node'])
 
-built_in_functions = [SequentialFunction('sqrtf', 'float', [Parameter(['float', 'value'])], True, None),
-                      SequentialFunction('cos', 'float', [Parameter(['float', 'value'])], True, None),
-                      SequentialFunction('sin', 'float', [Parameter(['float', 'value'])], True, None)
-                      ]
+built_in_functions = [SequentialFunction('sqrt',  'float', [Parameter(['float', 'value'])], True, None),
+                      SequentialFunction('sqrtf', 'float', [Parameter(['float', 'value'])], True, None),
+                      SequentialFunction('cos',   'float', [Parameter(['float', 'value'])], True, None),
+                      SequentialFunction('sin',   'float', [Parameter(['float', 'value'])], True, None)]
