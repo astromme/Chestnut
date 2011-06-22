9b9b39e7850240cea740ab3688486fca5e8d7e14
Andrew Stromme <astromme@chatonka.com>
2011-06-22 10:54:28 -0400

    remove old file parsing code

--- chestnut/ParseLibs.cpp
+++
@@ -1,7 +0,0 @@
-#include "ParseLibs.h"
-
-double totalTime(timeval* start, timeval* stop)
-{
-    return (stop->tv_sec + stop->tv_usec*0.000001)
-      - (start->tv_sec + start->tv_usec*0.000001);
-}
