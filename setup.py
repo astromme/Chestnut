a24a6926b0fea5354e5fa3fbfcf15096f82ebea7
Andrew Stromme <astromme@chatonka.com>
2012-01-28 20:56:54 -0500

    allow for the chestnut compiler to be installed systemwide via distutils

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='chestnut-compiler',
+      version='0.1',
+      description='Chestnut Compiler',
+      author='Andrew Stromme',
+      author_email='andrew.stromme@gmail.com',
+      url='http://chestnutcode.org/',
+      packages=['chestnut', 'chestnut.templates'],
+      package_data={'chestnut' : ['templates/*.cpp']},
+      scripts = ['scripts/chestnut-compiler',
+                 'scripts/chestnut-parser'],
+      long_description="""The Chestnut Compiler"""
+     )
